376e517eeb163f5068cdb9c6bfa7b0c12e84fea8
quinnHuang <quinn.huang@oocl.com>
2018-07-09 20:56:17 +0800

    [Quinn] complete practice 10

--- src/practice_10/class.js
+++ src/practice_10/class.js
@@ -1,1 +1,24 @@
-
+export default class Class {
+  constructor(number) {
+    this.number=number;
+  }
+  getDisplayName(){
+    return `Class ${this.number}`;
+  }
+  equal(klass){
+    return this.number===klass.number;
+  }
+  assignLeader(student){
+    if(this.equal(student.klass)){
+      this.leader=student;
+    }else {
+      console.log("It is not one of us.");
+    }
+  }
+  appendMember(student){
+    student.klass=this;
+  }
+  isIn(student){
+    return this.equal(student.klass);
+  }
+}
--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,28 @@
 
+import Person from "./person.js";
+export default class Teacher extends Person {
+  constructor(id,name,age,klasses=[]) {
+    super(id,name,age);
+    this.klasses=klasses;
+  }
+  introduce(){
+    return `${super.introduce()} I am a Teacher. ${this.class_introduce()}`;
+  }
+  class_introduce(){
+    return (this.klasses.length===0)
+      ?"I teach No Class."
+      :`I teach Class ${this.klasses.map( klass =>klass.number).join(', ')}.`;
+  }
+  introduceWith(student){
+    let isTeach=this.isTeaching(student)
+      ?"teach"
+      :"don't teach";
+    return `${super.introduce()} I am a Teacher. I ${isTeach} ${student.name}.`;
+  }
+  isTeaching(student){
+    return this.klasses.some((klass)=>{
+        return klass.isIn(student);
+      });
+    // return true;
+  }
+}
